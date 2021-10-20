be6130ec2202486dcbcae4a514da90ac2153b29a
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-10-20 20:20:22 -0300

    feat: Application Web Completed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,18 @@
-import styles from "./App.module.scss";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/auth";
 import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+
+import styles from "./App.module.scss";
 
 export function App() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <main className={styles.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ""}`}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
