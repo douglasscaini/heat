f10e297006a4c690b62a0e5c7743faafa7f29fef
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-10-20 17:43:37 -0300

    feat: Web Home Page Completed

---
+++ web/src/components/LoginBox/index.tsx
@@ -0,0 +1,14 @@
+import { VscGithubInverted } from "react-icons/vsc";
+import styles from "./styles.module.scss";
+
+export function LoginBox() {
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre o compartilhe sua mensagem</strong>
+      <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted />
+        Entrar com Github
+      </a>
+    </div>
+  );
+}
