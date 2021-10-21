fd1e196f15eddcb97b887168f1fa387053767c4c
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-10-21 17:30:26 -0300

    mobile: Component SendMessageForm

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,6 +4,7 @@ import { View } from "react-native";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
 import { styles } from "./styles";
 
@@ -13,7 +14,8 @@ export function Home() {
       <Header />
       <MessageList />
 
-      <SignInBox />
+      <SendMessageForm />
+      {/* <SignInBox /> */}
     </View>
   );
 }
