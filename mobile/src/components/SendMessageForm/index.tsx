9946e5fb51b2b8a99140f5587ea87d2c210cf27a
Douglas Scaini <douglasscaini.dev@gmail.com>
2021-10-22 10:43:51 -0300

    mobile: Mobile Completed

--- mobile/src/components/SendMessageForm/index.tsx
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
-import { TextInput, View } from "react-native";
-import { COLORS } from "../../theme";
+import { Alert, Keyboard, TextInput, View } from "react-native";
+import { api } from "../../services/api";
 import { Button } from "../Button";
 
+import { COLORS } from "../../theme";
 import { styles } from "./styles";
 
 export function SendMessageForm() {
   const [message, setMessage] = useState("");
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessagesSubmit() {
+    const messageFormatted = message.trim();
+
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true);
+
+      await api.post("/messages", { message: messageFormatted });
+
+      setMessage("");
+      Keyboard.dismiss();
+      setSendingMessage(false);
+
+      Alert.alert("Mensagem enviada com sucesso!");
+    } else {
+      Alert.alert("Escreva a mensagem para enviar.");
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -23,7 +42,13 @@ export function SendMessageForm() {
         editable={!sendingMessage}
       />
 
-      <Button title="ENVIAR MENSAGEM" backgroundColor={COLORS.PINK} color={COLORS.WHITE} />
+      <Button
+        title="ENVIAR MENSAGEM"
+        backgroundColor={COLORS.PINK}
+        color={COLORS.WHITE}
+        isLoading={sendingMessage}
+        onPress={handleMessagesSubmit}
+      />
     </View>
   );
 }
